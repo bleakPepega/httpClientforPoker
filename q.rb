ac852dcd7c936964fd7410a705119d8bbface18a
bleak_Pepega <matraskin879@gmail.com>
2022-10-10 05:34:14 +0300

    q

--- q.rb
+++ q.rb
@@ -1,12 +1,12 @@
 # http_server.rb
 require 'socket'
 server = TCPServer.new 5678
-
+base = ""
 while session = server.accept
   request = session.gets
   puts request
-
-  session.print(request)
+  base = request
+  session.print(base)
 
 
   session.close
